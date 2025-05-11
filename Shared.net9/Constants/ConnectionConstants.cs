0bd3f66a1ea12c7d6fe2cddc2c98e4508144a555
Zonakkis <2720716602@qq.com>
2025-05-12 00:51:00 +0800

    maybe fix cpu usage is too high

--- Shared.net9/Constants/ConnectionConstants.cs
+++ Shared.net9/Constants/ConnectionConstants.cs
@@ -5,7 +5,7 @@
         public const int MaxPlayers = 10;
         public const string Key = "GOILauncher";
         public const int Timeout = 5000;
-        public const int UpdateTime = 1;
+        public const int UpdateTime = 10;
         public const int MaxRetries = 1;
     }
 }
