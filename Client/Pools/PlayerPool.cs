b8177782147a92b604090a899f1e0b874abffb3a
Zonakkis <2720716602@qq.com>
2025-05-22 20:15:47 +0800

    Feat：使用对象池管理玩家GameObject

---
+++ Client/Pools/PlayerPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace GOILauncher.Multiplayer.Client.Pools
+{
+    public class PlayerPool
+    {
+        private readonly GameObject _playerPrefab;
+        private readonly Queue<GameObject> _players = new Queue<GameObject>();
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public PlayerPool(GameObject playerPrefab)
+        {
+            _playerPrefab = playerPrefab;
+        }
+
+        public GameObject Get()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                if (_players.Count > 0)
+                    return _players.Dequeue();
+                var player = Object.Instantiate(_playerPrefab);
+                player.SetActive(true);
+                return player;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public void Release(GameObject player)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                player.SetActive(false);
+                _players.Enqueue(player);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
