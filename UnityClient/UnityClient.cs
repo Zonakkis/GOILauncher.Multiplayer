345fb6ea80516df40be95f922c81eb16d1ff9b13
Zonakkis <2720716602@qq.com>
2026-03-22 15:53:40 +0800

    feat: 重构 UnityClient 类，添加依赖注入支持并更新项目引用

--- UnityClient/UnityClient.cs
+++ UnityClient/UnityClient.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Autofac;
+using GOILauncher.Multiplayer.Client.Extensions;
+using GOILauncher.Multiplayer.Client.Services;
+using UnityEngine;
 
 namespace GOILauncher.Multiplayer.Client
 {
-    internal class UnityClient
+    public class UnityClient : MonoBehaviour
     {
+        private IContainer _container;
+        private ILifetimeScope _scope;
+        private IClientService _clientService;
+        private void Awake()
+        {
+            var builder = new ContainerBuilder();
 
+            builder.RegisterMultiplayerClient();
+
+            _container = builder.Build();
+        }
+
+        private void OnDestroy()
+        {
+            _container.Dispose();
+        }
+
+        public void Connect(string host, int port)
+        {
+            _scope = _container.BeginLifetimeScope();
+            _clientService = _scope.Resolve<IClientService>();
+            _clientService.Connect(host, port);
+        }
+
+        public void Stop()
+        {
+            _scope?.Dispose();
+            _scope = null;
+        }
     }
 }
