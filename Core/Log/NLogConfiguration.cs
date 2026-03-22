1acfc23b0c0986837d13924aed5d6ece115a2fe5
Zonakkis <2720716602@qq.com>
2026-03-22 14:15:30 +0800

    feat: 添加 NLogLogger 和 ILogger 接口，配置 NLog 日志记录

---
+++ Core/Log/NLogConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOILauncher.Multiplayer.Core.Log
+{
+    public class NLogConfiguration
+    {
+        public static void Configure()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ConsoleTarget("console");
+            config.AddTarget(consoleTarget);
+
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, consoleTarget, "*");
+        }
+    }
+}
