a23ada8e04cb5453b23428b54639ec1b79fbef2b
Zonakkis <2720716602@qq.com>
2026-03-27 00:54:54 +0800

    feat: 更新 UnityClient 类，添加 PlayerService 属性，优化 Poll 调用方式

--- Unity/UnityClient.cs
+++ Unity/UnityClient.cs
@@ -1,4 +1,5 @@
-﻿using GOILauncher.Multiplayer.Client.Services;
+﻿using GOILauncher.Multiplayer.Client.Extensions;
+using GOILauncher.Multiplayer.Client.Services;
 using UnityEngine;
 
 namespace GOILauncher.Multiplayer.Client
@@ -6,20 +7,22 @@ namespace GOILauncher.Multiplayer.Client
     public class UnityClient : MonoBehaviour
     {
         public IClientService ClientService { get; set; }
+        public IPlayerService PlayerService { get; set; }
 
         private void Update()
         {
-            ClientService.Poll();
+            ClientService?.Poll();
         }
 
-        public void Connect(string host, int port)
+        public void Connect(string host, int port, PlayerMetadata playerMetadata)
         {
+            PlayerService.UpdateLocalPlayerMetadata(playerMetadata);
             ClientService.Connect(host, port);
         }
 
         public void Disconnect()
         {
-            ClientService.Disconnect();
+            ClientService?.Disconnect();
         }
     }
 }
