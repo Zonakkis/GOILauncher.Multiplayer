99680bdaefa48d49dc56724811549e8408e1a0f1
Zonakkis <2720716602@qq.com>
2026-03-28 00:00:37 +0800

    feat: 添加 ILogger 接口及其 NLog 实现，增强日志记录功能

---
+++ src/Core/Log/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOILauncher.Multiplayer.Core.Log
+{
+    public interface ILogger<T>
+    {
+        void Info(string message);
+        void Info(string message, params object[] args);
+        void Debug(string message);
+        void Debug(string message, params object[] args);
+        void Warn(string message);
+        void Warn(string message, params object[] args);
+        void Error(string message, Exception ex = null);
+        void Error(string message, Exception ex = null, params object[] args);
+    }
+}
---
+++ src/Core/Log/NLogLogger.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+using System;
+
+namespace GOILauncher.Multiplayer.Core.Log
+{
+    public class NLogLogger<T> : ILogger<T>
+    {
+        private readonly Logger _logger;
+
+        public NLogLogger()
+        { 
+            _logger = LogManager.GetLogger(typeof(T).FullName);
+        }
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+        public void Info(string message, params object[] args)
+        {
+            _logger.Info(message, args);
+        }
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+        public void Debug(string message, params object[] args)
+        {
+            _logger.Debug(message, args);
+        }
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+        public void Warn(string message, params object[] args)
+        {
+            _logger.Warn(message, args);
+        }
+        public void Error(string message, Exception ex = null)
+        {
+            _logger.Error(message, ex);
+        }
+        public void Error(string message, Exception ex = null, params object[] args)
+        {
+            _logger.Error(ex, message, args);
+        }
+    }
+}
