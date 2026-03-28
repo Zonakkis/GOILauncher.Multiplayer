a1a9b98075b4c834ee53ec6dbc76cec49a9fab22
Zonakkis <2720716602@qq.com>
2026-03-29 00:48:47 +0800

    feat: 添加桌面BepInEx项目

---
+++ src/Unity.Desktop/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace GOILauncher.Multiplayer;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity calls this method via reflection.")]
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
