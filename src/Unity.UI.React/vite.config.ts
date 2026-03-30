86e626799d8280bce81bebf73c2dc745b74b5954
Zonakkis <2720716602@qq.com>
2026-03-30 22:26:21 +0800

    chore:输出单文件使能运行在chromium74中

--- src/Unity.UI.React/vite.config.ts
+++ src/Unity.UI.React/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: 'es2015',
+    modulePreload: false,
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        inlineDynamicImports: true,
+      }
+    }
+  }
 })
