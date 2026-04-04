eace8bca3ead59c01ecf3bcb69bc0e00ac8253fc
Zonakkis <2720716602@qq.com>
2026-04-04 17:29:20 +0800

    feat: 添加BepInExTarget类以支持NLog日志记录功能，并在项目中引入NLog依赖

---
+++ src/Unity.Desktop/BepInExTarget.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx.Logging;
+using NLog;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOILauncher.Multiplayer
+{
+    [Target("BepInExTarget")]
+    public class BepInExTarget : TargetWithLayout
+    {
+        private ManualLogSource _logger;
+
+        public BepInExTarget(ManualLogSource logger)
+        {
+            _logger = logger;
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = Layout.Render(logEvent);
+            switch (logEvent.Level.Name)
+            {
+                case "Trace":
+                case "Debug":
+                    _logger.LogDebug(logMessage);
+                    break;
+                case "Info":
+                    _logger.LogInfo(logMessage);
+                    break;
+                case "Warn":
+                    _logger.LogWarning(logMessage);
+                    break;
+                case "Error":
+                    _logger.LogError(logMessage);
+                    break;
+                case "Fatal":
+                    _logger.LogFatal(logMessage);
+                    break;
+                default:
+                    _logger.LogInfo(logMessage);
+                    break;
+            }
+        }
+    }
+}
